494258c557dee513bdf861fd522c5fcfb195cb66
OmarHani4306 <109454477+OmarHani4306@users.noreply.github.com>
2024-12-04 03:51:22 +0200

    Update DestinationRepository.java
    updated tourism_type query

--- PathPortDB/src/main/java/com/SWEProject/PathPortDB/repository/DestinationRepository.java
+++ PathPortDB/src/main/java/com/SWEProject/PathPortDB/repository/DestinationRepository.java
@@ -10,14 +10,20 @@ import java.util.List;
 
 @Repository
 public interface DestinationRepository extends MongoRepository<Destination, String> {
-
-
+    
     @Query("{ '_id': ?0 }")
     Destination findDestinationById(String id);
 
-    @Query(value = "{}", fields = "{'title': 1, 'photoUrl': 1, 'price': 1, 'location': 1, 'rating': 1, 'description': 1}")
+    @Query(
+        value = "{}",
+        fields = "{'title': 1, 'photoUrl': 1, 'price': 1, 'location': 1, 'rating': 1, 'description': 1}"
+    )
     List<Destination> findAllWithSpecificFields();
 
-    @Query(value = "{ 'tourismType': ?0 }", fields = "{'title': 1, 'photoUrl': 1, 'price': 1, 'location': 1, 'rating': 1, 'description': 1}")
+    @Query(
+        value = "{ 'tourism_type': { $in: [?0] } }",
+        fields = "{'title': 1, 'photo_Url': 1, 'price': 1, 'location': 1, 'rating': 1, 'description': 1}"
+    )
     List<Destination> findByTourismTypeWithSpecificFields(String tourismType);
 }
+
